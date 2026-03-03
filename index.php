bc67aca6195cbed43ab797f9b45be79f86d5d0c4
jado150 <jeandedieuiyandemye@example.com>
2026-03-03 07:13:51 -0800

    Mobile Support for better responsive

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TourStack Booking</title>
     <link rel="stylesheet" href="main.css">
 </head>
